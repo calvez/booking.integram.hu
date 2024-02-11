d487d006121aa9df150028758840a60ec94b474d
calvez <nemeth.lorant@gmail.com>
2024-02-11 08:50:05 +0100

    dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Az Ön foglalásai!") }}
+                    {{ __("Az Ön foglalásai és fizetési előzményei itt lesznek láthatóak!") }}
                 </div>
             </div>
         </div>
